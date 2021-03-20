2c1345636fce250de57fbe3fe498be3c3ffda7fd
Rajat Kumar <991rajat@gmail.com>
2021-03-20 21:00:10 +0530

    added footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import Header from './component/header/Header';
 import Main from './component/landing/Main';
+import Footer from './component/footer/Footer';
 const App = () => {
   return (
     <div className="App">
       <Header></Header>
       <Main></Main>
+      <Footer></Footer>
     </div>
   );
 };
